cf5209b5c7a5f2f5e288f3e3d1c7a64053be4b4a
cjs0410 <jinsuchoi@kaist.ac.kr>
2025-02-10 15:04:55 +0900

    Convert output type (stdout -> JSON)

--- pcov.py
+++ pcov.py
@@ -4,20 +4,28 @@ import sys
 from copy import deepcopy
 import test_pcov
 
-def print_result(verbose:bool, stmt_covered:int, stmt_total:int, stmt_missing:list[str], branch_covered:int, branch_total:int, branch_missing:list[str]):
-	
+def save_result(target, verbose:bool, stmt_covered:int, stmt_total:int, stmt_missing:list[str], branch_covered:int, branch_total:int, branch_missing:list[str]):
+	#Please satisfy the type of each parameters
+	result_path = "./pcov.json"
 	stmt_coverage = 0 if stmt_total == 0 else stmt_covered / stmt_total * 100
 	branch_coverage = 0 if branch_total == 0 else branch_covered / branch_total * 100
 
-	print("=====================================")
-	print("Statement Coverage: {0:.2f} ({1}/{2})".format(stmt_coverage, stmt_covered, stmt_total))
+	result = {}
+	result["target_file"] = target
+	result["covered_lines"] = stmt_covered
+	result["num_statements"] = stmt_total
+	result["statement_coverage"] = format(stmt_coverage, ".2f")
 	if verbose:
-		print("Missing Statements: {}".format(", ".join([str(line_num) for line_num in stmt_missing])))
-	print("=====================================")
-	print("Branch Coverage: {0:.2f} ({1}/{2})".format(branch_coverage, branch_covered, branch_total))
+		result["missing_lines"] = stmt_missing
+	
+	result["covered_branches"] = branch_covered
+	result["num_branches"] = branch_total
+	result["branch_coverage"] = format(branch_coverage, ".2f")
 	if verbose:
-		print("Missing Branches: {}".format(", ".join([str(line_num) for line_num in branch_missing])))
-	print("=====================================")
+		result["missing_branches"] = branch_missing
+
+	with open(result_path, 'w') as f:
+		json.dump(result, f, indent=4)
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Measures coverage.')
@@ -40,4 +48,4 @@ if __name__ == '__main__':
 	# collect coverage
 	# ...
 
-	print_result(args.verbose, stmt_covered, stmt_total, stmt_missing, branch_covered, branch_total, branch_missing)
+	save_result(target, args.verbose, stmt_covered, stmt_total, stmt_missing, branch_covered, branch_total, branch_missing)
